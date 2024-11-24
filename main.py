4e659227b859f09fdfc70ce481c8d37d1bfe117b
rahmakal <rahma.kallel.89@gmail.com>
2024-11-24 12:29:01 +0100

    update main

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from src.model import submit
 
 dataset = "test"
 
-axis_info = pd.read_parquet("data/axis_info.parquet")
 preprocessed_signal = preprocess_signals(dataset)
 print(preprocessed_signal.shape)
 
 predictions_spectra = predict(preprocessed_signal)
 
 submit(predictions_spectra)
+
+
